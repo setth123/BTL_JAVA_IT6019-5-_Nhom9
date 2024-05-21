90bf3807d63fe988197cf6960255cb15c288f754
nguyenphuong29082004 <nguyenphuong29082004@gmail.com>
2024-05-21 14:30:49 +0700

    Statics

---
+++ BTL_JAVA_IT60195_NHOM9/src/backend/controllers/Statics.java
@@ -0,0 +1,13 @@
+package backend.controllers;
+
+import backend.utils.ReadData;
+
+public class Statics {
+	public static int books() {
+		return ReadData.readBook("/BTL_JAVA_IT60195_NHOM9/src/backend/DemoDB/Book.txt").size();
+		
+	}
+	public static int users() {
+		return ReadData.readAccount("/BTL_JAVA_IT60195_NHOM9/src/backend/DemoDB/Book.txt").size();
+	}
+}
