7aa155b59d944fe8f2f7fe74fc361d2d83607547
Edward <thanhtkcb2004@gmail.com>
2024-05-21 14:38:59 +0700

    Update ReadData.java

--- BTL_JAVA_IT60195_NHOM9/src/backend/utils/ReadData.java
+++ BTL_JAVA_IT60195_NHOM9/src/backend/utils/ReadData.java
@@ -3,6 +3,7 @@ package backend.utils;
 import backend.models.*;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.text.ParseException;
@@ -11,17 +12,26 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 public class ReadData {
+	
+	//convert relative path to absolute path.Ex: fileName="/BTL_JAVA_IT60195_NHOM9/src/backend/DemoDB/Librarian.txt"
+	public static String f_path(String fileName) {
+		String projectDirectory = System.getProperty("user.dir");
+        File file=new File(projectDirectory,fileName);
+        return file.getAbsolutePath();
+	}
+	
     public static List<Account> readAccount(String fileName) {
         List<Account> accounts = new ArrayList<>();
-
-        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+        String path=f_path(fileName);
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
             String line;
             while ((line = br.readLine()) != null) {
                 line = line.substring(1, line.length() - 1);
                 String[] data = line.split("\\|");
-                Account account = new Account(data[0].trim(), data[1].trim(), data[2].trim(), data[3].trim(), data[4].trim(), data[5].trim());
+                Account account = new Account(data[0].trim(), data[1].trim(), data[2].trim(), data[3].trim(), data[4].trim(), data[5].trim(), data[6].trim());
                 accounts.add(account);
             }
         } catch (IOException e) {
@@ -32,14 +42,20 @@ public class ReadData {
     }
 
     public static List<Book> readBook(String fileName) {
+    	
         List<Book> books = new ArrayList<>();
+        
+        String path=f_path(fileName);
 
-        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+        
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
             String line;
             while ((line = br.readLine()) != null) {
                 String[] data = line.split("\\|");
-                LocalDate nph = LocalDate.parse(data[4].trim());
-                Book book = new Book(data[0].trim(), data[1].trim(), data[2].trim(), data[3].trim(), nph, data[5].trim(), Integer.parseInt(data[6].trim()), Double.parseDouble(data[7].trim()));
+                Boolean isBorrow;
+                if(data[7].trim().equals("true"))isBorrow=true;
+                else isBorrow=false;
+                Book book = new Book(data[0].trim(), data[1].trim(), data[2].trim(), data[3].trim(), data[4].trim(), Integer.parseInt(data[5].trim()), Double.parseDouble(data[6].trim()),isBorrow);
                 books.add(book);
             }
         } catch (IOException e) {
@@ -51,8 +67,11 @@ public class ReadData {
 
     public static List<BorrowSlip> readBorrowSlip(String fileName) {
         List<BorrowSlip> slips = new ArrayList<>();
+        
+        String path=f_path(fileName);
 
-        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+        
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
             String line;
             while ((line = br.readLine()) != null) {
                 String[] data = line.split("\\|");
@@ -68,8 +87,10 @@ public class ReadData {
 
     public static List<BorrowSlipDetail> readBorrowSlipDetail(String fileName) {
         List<BorrowSlipDetail> details = new ArrayList<>();
+        
+        String path=f_path(fileName);
 
-        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
             String line;
             while ((line = br.readLine()) != null) {
                 String[] data = line.split("\\|");
@@ -86,8 +107,9 @@ public class ReadData {
 
     public static List<Category> readCategory(String fileName) {
         List<Category> categories = new ArrayList<>();
+        String path=f_path(fileName);
 
-        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
             String line;
             while ((line = br.readLine()) != null) {
                 String[] data = line.split("\\|");
@@ -103,8 +125,9 @@ public class ReadData {
 
     public static List<Violation> readViolation(String fileName) {
         List<Violation> violations = new ArrayList<>();
+        String path=f_path(fileName);
 
-        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
             String line;
             while ((line = br.readLine()) != null) {
                 String[] data = line.split("\\|");
@@ -117,5 +140,21 @@ public class ReadData {
         }
 
         return violations;
+    }
+    
+    public static List<Librarian> readLibrarian(String fileName) {
+        List<Librarian> librarians = new ArrayList<>();
+        String path=f_path(fileName);
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] data = line.split("\\|");
+                Librarian librarian = new Librarian(data[0].trim(), data[1].trim());
+                librarians.add(librarian);
+            }
+        } catch (IOException e) {
+            e.printStackTrace(System.err);
+        }
+        return librarians;
     }
 }
